471861e49ba72b7b4b5079bd08ff5d7f1a675ebe
wisnunugraha <wisnunugraha@yahoo.com>
2022-07-09 16:55:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [
+        {id: 1, name: "Fast 1", "year":2021},
+        {id: 2, name: "Fast 2", "year":2022},
+        {id: 3, name: "Fast 3", "year":2023},
+      ]
+    };
+  }
+
+  changeName = (e) => {
+    this.setState({
+      movies: [
+        {id: 1, name: e.target.value, "year":2021},
+        {id: 2, name: "Fast 2", "year":2022},
+        {id: 3, name: "Fast 3", "year":2023},
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Learn About React js</h1>
+        {
+          this.state.movies.map((movie) => {
+            return <h2 key={movie.id}>My Favofite movie is {movie.name}</h2>
+          })
+        }
+        <button onClick={() => { this.changeName()
+        }}>fast 11</button>
+        <input type="text" onChange={this.changeName} />
+      </div>
+    );
+  }
 }
 
 export default App;
