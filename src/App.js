9bb7a1fba4446ffd15c168e34eb4a289d390e731
bilabror <muhammadalhiqny@gmail.com>
2022-07-15 15:58:19 +0700

    try tailwind

--- src/App.js
+++ src/App.js
@@ -1,66 +1,80 @@
-import { Component } from "react";
+import {
+    Component
+} from "react";
 import './App.css';
 import PostList from "./Components/post-list"
 
 class App extends  Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      posts: [],
-      showPosts: false,
-      searchInput: ''
-    };
-  }
+        this.state = {
+            posts: [],
+            showPosts: false,
+            searchInput: ''
+        };
+    }
 
-  changeName = (e) => {
-    this.setState({
-      movies: [],
-    });
-  }
+    changeName = (e) => {
+        this.setState({
+            movies: [],
+        });
+    }
 
-  componentDidMount() {
-    fetch("https://dummyjson.com/posts")
-    .then((response) => response.json())
-    .then((api) => 
-    this.setState(() => {
-      return {posts: api.posts, showPosts: true};
-    })
-    );
-  }
+    componentDidMount() {
+        fetch("https://dummyjson.com/posts")
+        .then((response) => response.json())
+        .then((api) =>
+            this.setState(() => {
+                return {
+                    posts: api.posts, showPosts: true
+                };
+            })
+        );
+    }
 
-  searchPosts = (e) => {
-    const search = e.target.value.toLowerCase();
-    this.setState(() => {
-      return {searchInput: search}
-    })
-  }
+    searchPosts = (e) => {
+        const search = e.target.value.toLowerCase();
+        this.setState(() => {
+            return {
+                searchInput: search
+            }
+        })
+    }
 
-  render() {
-    let {showPosts, searchInput, posts} = this.state;
-    const filterPosts = posts.filter((post) => {
-      return post.title.toLowerCase().includes(searchInput);
-    });
-    let renderPosts = "Loading Posts ...";
+    render() {
+        let {
+            showPosts,
+            searchInput,
+            posts
+        } = this.state;
+        const filterPosts = posts.filter((post) => {
+            return post.title.toLowerCase().includes(searchInput);
+        });
+        let renderPosts = "Loading Posts ...";
 
 
-    if(showPosts) renderPosts = <PostList posts={filterPosts}/>
+        if (showPosts) renderPosts = <PostList posts={filterPosts} />
 
-    return (
-      <div className="App">
-        <h1>BLOG POSTS <button onClick={() => {
-          this.setState({showPosts: !showPosts})
-        }}>SHOW POSTS</button></h1>
-          <input
-            type="text"
-            placeholder="Search Posts"
-            onChange={this.searchPosts} />
-          
-        {renderPosts}
-       
-      </div>
-    );
-  }
+        return (
+            <div className="container mt-8 mx-auto">
+        <h1 className="text-center font-bold text-xl text-white">BLOG POSTS <button onClick={() => {
+                this.setState({ showPosts: !showPosts })
+            }}>SHOW POSTS</button></h1>
+                <form className="flex items-center w-5/12 mx-auto mt-3">
+    <label for="simple-search" className="sr-only">Search</label>
+        <div className="relative w-full">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+                <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" onChange={this.searchPosts} required="" />
+            </div>
+                <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+            </form>
+                {renderPosts}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/post-list.jsx
+++ src/Components/post-list.jsx
@@ -1,21 +1,30 @@
-import { Component} from 'react';
+import {
+    Component
+} from 'react';
 
 class PostList extends Component {
-  render() {
-    return (
-      <div className="cardContainer">
+    render() {
+        return (
+            <div className="py-4 flex flex-wrap justify-center">
           {
-              this.props.posts.map((post) => {
-                return (
-                  <div className="card" key={post.id}>
-                    <h2 className="titlecard">{post.title}</h2>
-                  </div>
-                )
-              })
-          }
-        </div>
-    )
-  }
+                this.props.posts.map((post) => {
+                    return (
+                        <div className="md:w-3/12 sm:w-4/12 m-3 bg-white rounded overflow-hidden shadow-lg" key={post.id}>
+                        <img className="w-full" src="https://source.unsplash.com/random/200x100?sig=tecnology" />
+                        <div className="p-2">
+                            <h2 className="font-bold text-xl mb-2">{post.title.substring(0, 20)}...</h2>
+                            <p className="text-gray-600">
+                            {post.body.substring(0, 120)}...
+                        </p>
+                        <a href="" className="mt-3 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more >></a>
+                        </div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+        )
+    }
 }
 
 export default PostList;
